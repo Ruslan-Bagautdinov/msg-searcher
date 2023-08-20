6f0e2ed2d89aac25f52e8b8ae49a124b402e24ab
ruslan3lodey <ruslan3lodey@gmail.com>
2023-08-20 13:35:09 +0300

    ExSearcherTur v.0.0.6
    working 100%
    server 0%
    
    ready for Heroku deploy

--- bot_creator.py
+++ bot_creator.py
@@ -1,16 +1,6 @@
 from aiogram import Bot
-from os import getenv
 
-SERVER = getenv('SERVER', False)
-
-if SERVER:
-    BOT_TOKEN = getenv('BOT_TOKEN')
-    GROUP_CHAT_ID = getenv('GROUP_CHAT_ID')
-
-else:
-    from config import *
-    BOT_TOKEN = BOT_TOKEN
-    GROUP_CHAT_ID = GROUP_CHAT_ID
+from searcher import BOT_TOKEN
 
 bot = Bot(
           token=BOT_TOKEN
--- searcher.py
+++ searcher.py
@@ -1,11 +1,27 @@
 from pyrogram import Client, filters
 import re
+from os import getenv
 
 from icecream import ic
 
 # own imports
-from config import APP_NAME, SESSION_STRING
 from sender import send_to_group
+
+SERVER = getenv('SERVER', False)
+
+if SERVER:
+    BOT_TOKEN = getenv('BOT_TOKEN')
+    GROUP_CHAT_ID = getenv('GROUP_CHAT_ID')
+    APP_NAME = getenv('APP_NAME')
+    SESSION_STRING = getenv('SESSION_STRING')
+
+else:
+    from config import BOT_TOKEN, GROUP_CHAT_ID, APP_NAME, SESSION_STRING
+    BOT_TOKEN = BOT_TOKEN
+    GROUP_CHAT_ID = GROUP_CHAT_ID
+    APP_NAME = APP_NAME
+    SESSION_STRING = SESSION_STRING
+
 
 chats = ['test_Alexandra_Exchange',
          '+jzWUJ4kYorQ4YWI6',
