6fd96814bfa94fd175ec63ce1cc065f3c64d9c52
ruslan3lodey <ruslan3lodey@gmail.com>
2023-08-20 13:17:57 +0300

    ExSearcherTur v.0.0.5
    working 100%
    server 0%
    
    ready for Heroku deploy

--- bot_creator.py
+++ bot_creator.py
@@ -5,15 +5,11 @@ SERVER = getenv('SERVER', False)
 
 if SERVER:
     BOT_TOKEN = getenv('BOT_TOKEN')
-    URI_APP = getenv('URI_APP')
-    GROUP_CHAT_NAME = getenv('GROUP_CHAT_NAME')
     GROUP_CHAT_ID = getenv('GROUP_CHAT_ID')
 
 else:
     from config import *
     BOT_TOKEN = BOT_TOKEN
-    URI_APP = URI_APP
-    GROUP_CHAT_NAME = GROUP_CHAT_NAME
     GROUP_CHAT_ID = GROUP_CHAT_ID
 
 bot = Bot(
--- sender.py
+++ sender.py
@@ -3,6 +3,6 @@ from bot_creator import bot, GROUP_CHAT_ID
 
 async def send_to_group(message_for_bot):
 
-    await bot.send_message(GROUP_CHAT_ID,
+    await bot.send_message(int(GROUP_CHAT_ID),
                            message_for_bot
                            )
